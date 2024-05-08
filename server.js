00feb3f38fcf53e731813ae4cfeada96077fe800
Erik <rainer.erik70@gmail.com>
2024-05-08 14:31:03 -0700

    server changes made

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const helpers = require('./utils/helpers');
 
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 // Set up sessions
 const sess = {
@@ -31,7 +35,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
