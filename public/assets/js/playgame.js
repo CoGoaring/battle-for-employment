b176c6b4de24b3919f8061515923531e15d2d148
Erik <rainer.erik70@gmail.com>
2024-05-09 22:34:57 -0700

    fixed the issue where the user could pass the boss just not selectin a radio

--- public/assets/js/playgame.js
+++ public/assets/js/playgame.js
@@ -1,25 +1,39 @@
-let progress = document.getElementById("progress").value
-async function handleScoring (e){
-    e.preventDefault()
-    let score=0
-    const radios=document.querySelectorAll("input[type='radio']:checked")
-    radios.forEach(radio=>{
-        const{name,value}=radio
-        console.log(name,value)
-        if(name===value){score++}
-    })
-    if(score===radios.length){
-        progress++
-        const response=await fetch("/api/users",{method:"PUT",body:JSON.stringify({progress}),headers:{
-            "Content-Type":"application/json"
-        }})
-        if(response.ok){
-            document.location.reload()
-        }//replace to winning page
-    }else{alert("try again")
+let progress = document.getElementById("progress").value;
+async function handleScoring(e) {
+    e.preventDefault();
+    let score = 0;
+    const radios = document.querySelectorAll("input[type='radio']:checked");
+    if (radios.length === 0) {
+        alert("Please select an option");
+        return;
+    }
+    radios.forEach(radio => {
+        const { name, value } = radio;
 
+        if (name === value) {
+            score++;
+        }
+    });
+
+    try {
+        if (score === radios.length) {
+            progress++;
+            const response = await fetch("/api/users", {
+                method: "PUT",
+                body: JSON.stringify({ progress }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            if (response.ok) {
+                document.location.reload();    
+            }
+        } else {
+            alert("It`s not very effective...")
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
 
-}
-
-document.querySelector("form").addEventListener("submit",handleScoring)
+document.querySelector("form").addEventListener("submit", handleScoring);
